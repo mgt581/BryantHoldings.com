73931e6b5e09d4825cc7d323decc9f7e43ced6f0
mgt581 <alexbryantwork3234@outlook.com>
2026-08-14 05:07:38 +0100

    Add preview lead generation dashboard

---
+++ site-config.js
@@ -0,0 +1,8 @@
+window.LEADGEN_CONFIG = {
+  businessName: 'Bryant Group Holdings',
+  storagePrefix: 'bryant_group_holdings',
+  leadEndpoint: '/api/lead',
+  eventEndpoint: '/api/lead-event',
+  dashboardEndpoint: '/api/dashboard',
+  googleTagManagerId: ''
+};
