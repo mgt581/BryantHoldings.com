f14e6812c7aaefcc9fd9f4a36b0cc90bf2b73e50
mgt581 <alexbryantwork3234@outlook.com>
2026-08-14 05:43:00 +0100

    Revert accidental preview merge from production

--- site-config.js
+++
@@ -1,8 +0,0 @@
-window.LEADGEN_CONFIG = {
-  businessName: 'Bryant Group Holdings',
-  storagePrefix: 'bryant_group_holdings',
-  leadEndpoint: '/api/lead',
-  eventEndpoint: '/api/lead-event',
-  dashboardEndpoint: '/api/dashboard',
-  googleTagManagerId: ''
-};
